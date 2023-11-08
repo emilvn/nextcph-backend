9b1bea9a9ed5869e182841c9cab37c8e3e1068da
emilvn <emilvnielsen@hotmail.com>
2023-11-08 21:04:18 +0100

    changed to use tabs

--- src/Server.ts
+++ src/Server.ts
@@ -2,31 +2,31 @@ import type {Application, RequestHandler} from 'express';
 import type Controller from './controllers/Controller';
 
 class Server{
-  private app: Application;
-  private readonly port: number;
+	private app: Application;
+	private readonly port: number;
 
-  constructor(app:Application, port: number) {
-    this.port = port;
-    this.app = app;
-  }
+	constructor(app:Application, port: number) {
+		this.port = port;
+		this.app = app;
+	}
 
-  public run(): void {
-    this.app.listen(this.port, () => {
-      console.log(`Server started on port http://localhost:${this.port}`);
-    });
-  }
+	public run(): void {
+		this.app.listen(this.port, () => {
+  			console.log(`Server started on port http://localhost:${this.port}`);
+		});
+	}
 
-  public loadMiddleware(middlewares: RequestHandler[]): void {
-    middlewares.forEach((middleware) => {
-        this.app.use(middleware);
-    });
-  }
+	public loadMiddleware(middlewares: RequestHandler[]): void {
+		middlewares.forEach((middleware) => {
+  			this.app.use(middleware);
+		});
+	}
 
-    public loadControllers(controllers:Controller[]): void {
-        controllers.forEach((controller) => {
-            this.app.use(controller.path, controller.router);
-        });
-    }
+	public loadControllers(controllers:Controller[]): void {
+		controllers.forEach((controller) => {
+  			this.app.use(controller.path, controller.router);
+		});
+	}
 }
 
 export default Server;
