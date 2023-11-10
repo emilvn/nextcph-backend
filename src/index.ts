ccda5e483ecdeada0b2688ebbc3c532deaf17136
emilvn <emilvnielsen@hotmail.com>
2023-11-10 12:32:12 +0100

    update port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import ProductController from "./controllers/ProductController";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const server = new Server(app, 3000);
+const port = Number(process.env.PORT) || 3000;
+const server = new Server(app, port);
 const db = new PrismaClient();
 
 const controllers = [
