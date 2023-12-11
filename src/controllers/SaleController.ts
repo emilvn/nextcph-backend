858d0fc175f53a33fe188b95a201ac9ecda709f3
emilvn <emilvnielsen@hotmail.com>
2023-12-11 15:13:31 +0100

    validation

--- src/controllers/SaleController.ts
+++ src/controllers/SaleController.ts
@@ -4,9 +4,14 @@ import type { Request, Response, NextFunction } from "express";
 import type { PrismaClient } from "@prisma/client";
 import type { INewSale } from "../types/types";
 import SaleRepository from "../repositories/SaleRepository";
-import {RequiredChannelSchema, NewSaleSchema, DateSchema, OptionalChannelSchema} from "../validation/schemas";
+import {
+    RequiredChannelSchema,
+    NewSaleSchema,
+    DateSchema,
+    OptionalChannelSchema
+} from "../validation/schemas";
 import CategoryRepository from "../repositories/CategoryRepository";
-import {getAmountOfDaysInMonth} from "../helpers/dates";
+import { getAmountOfDaysInMonth } from "../helpers/dates";
 
 class SaleController extends Controller {
     path: string = "/sales";
@@ -16,62 +21,79 @@ class SaleController extends Controller {
         this.repository = new SaleRepository(db);
     }
 
-    public getAll = async (req:Request, res:Response, next:NextFunction) => {
-        try{
-            const {channel, page, pageSize, user_id} = req.query;
-			const userIdParam = user_id as string | undefined;
+    public getAll = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { channel, page, pageSize, user_id } = req.query;
+            const userIdParam = user_id as string | undefined;
             const channelParam = OptionalChannelSchema.parse(channel);
 
-            const salesData = await this.repository
-				.getAll(channelParam, Number(page), Number(pageSize), userIdParam);
-            if(salesData.data.length === 0) {
+            const salesData = await this.repository.getAll(
+                channelParam,
+                Number(page),
+                Number(pageSize),
+                userIdParam
+            );
+            if (salesData.data.length === 0) {
                 res.status(204).send("No sales yet");
-            }
-            else{
+            } else {
                 res.json(salesData);
             }
         } catch (e) {
             next(e);
         }
-    }
+    };
 
-    public getByMonth = async (req: Request, res: Response, next: NextFunction) => {
+    public getByMonth = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
         try {
             const { channel } = req.query;
             const channelParam = RequiredChannelSchema.parse(channel);
             let { month } = req.query;
             if (typeof month !== "string") {
                 const now = new Date();
-                month = new Date(now.getFullYear(), now.getMonth() - 1, 1).toISOString();
+                month = new Date(
+                    now.getFullYear(),
+                    now.getMonth() - 1,
+                    1
+                ).toISOString();
             }
 
-			const monthParam = DateSchema.parse(month);
-			const sales = await this.repository.getByMonth(monthParam, channelParam);
-			if (sales.length === 0) {
-				res.status(204).send(sales);
-			}else{
+            const monthParam = DateSchema.parse(month);
+            const sales = await this.repository.getByMonth(
+                monthParam,
+                channelParam
+            );
+            if (sales.length === 0) {
+                res.status(204).send(sales);
+            } else {
                 res.json(sales);
             }
-		} catch (e) {
-			next(e);
-		}
-	}
+        } catch (e) {
+            next(e);
+        }
+    };
 
-    public getById = async (req: Request, res: Response, next: NextFunction) => {
+    public getById = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
         try {
             const { id } = req.params;
             const sale = await this.repository.getById(id);
 
             if (!sale) {
                 res.status(404).send("No sale found with id: " + id);
-            }
-            else {
+            } else {
                 res.json(sale);
             }
         } catch (e) {
             next(e);
         }
-    }
+    };
 
     public create = async (req: Request, res: Response, next: NextFunction) => {
         try {
@@ -81,18 +103,23 @@ class SaleController extends Controller {
         } catch (e) {
             next(e);
         }
-    }
+    };
 
-    public createMany = async (req: Request, res: Response, next: NextFunction) => {
+    public createMany = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
         try {
-            const data: INewSale[] = req.body.map((sale: INewSale) => NewSaleSchema.parse(sale));
+            const data: INewSale[] = req.body.map((sale: INewSale) =>
+                NewSaleSchema.parse(sale)
+            );
             const sales = await this.repository.createMany(data);
             res.status(201).json(sales);
         } catch (e) {
             next(e);
         }
-
-    }
+    };
 
     public delete = async (req: Request, res: Response, next: NextFunction) => {
         try {
@@ -102,9 +129,13 @@ class SaleController extends Controller {
         } catch (e) {
             next(e);
         }
-    }
+    };
 
-    public getStatistics = async (req: Request, res: Response, next: NextFunction) => {
+    public getStatistics = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
         try {
             const { channel } = req.query;
             const channelParam = RequiredChannelSchema.parse(channel);
@@ -112,18 +143,26 @@ class SaleController extends Controller {
             let { month } = req.query;
             if (typeof month !== "string") {
                 const now = new Date();
-                month = new Date(now.getFullYear(), now.getMonth()).toISOString();
+                month = new Date(
+                    now.getFullYear(),
+                    now.getMonth()
+                ).toISOString();
             }
 
             const monthParam = DateSchema.parse(month);
             monthParam.setDate(1);
-            const rawSalesData = await this.repository.getByMonth(monthParam, channelParam);
+            const rawSalesData = await this.repository.getByMonth(
+                monthParam,
+                channelParam
+            );
 
             let categories: { name: string; total?: number }[] = [];
-            const categoryRepository = new CategoryRepository(this.repository.db);
+            const categoryRepository = new CategoryRepository(
+                this.repository.db
+            );
             categories = await categoryRepository.getNames(channelParam);
 
-            let totalSales = 0 ;
+            let totalSales = 0;
             let totalRevenue = 0;
 
             rawSalesData.forEach((sale) => {
@@ -133,29 +172,37 @@ class SaleController extends Controller {
                     const product = saleProduct.product;
                     const matchingCategory = categories.find((category) => {
                         const currentCategoryName = category.name;
-                        return product?.categories.some((c) => c.category.name === currentCategoryName)
+                        return product?.categories.some(
+                            (c) => c.category.name === currentCategoryName
+                        );
                     });
                     if (matchingCategory) {
                         const productPrice = product?.price || 0;
-                        const productQuantity = saleProduct.product_quantity || 0;
+                        const productQuantity =
+                            saleProduct.product_quantity || 0;
 
-                        matchingCategory.total = (matchingCategory.total || 0) + (productPrice * productQuantity);
+                        matchingCategory.total =
+                            (matchingCategory.total || 0) +
+                            productPrice * productQuantity;
                         totalRevenue += productPrice * productQuantity;
                     }
                 });
             });
 
-            totalRevenue = categories.reduce((acc, category) => (acc + (category.total || 0)), 0);
+            totalRevenue = categories.reduce(
+                (acc, category) => acc + (category.total || 0),
+                0
+            );
 
             const daysInMonth = getAmountOfDaysInMonth(monthParam);
 
             const averageDailySales = totalSales / daysInMonth;
             const averageDailyRevenue = totalRevenue / daysInMonth;
 
-            const categoriesWithPercentage = categories.map(category => ({
+            const categoriesWithPercentage = categories.map((category) => ({
                 name: category.name,
                 total: category.total || 0,
-                percentage: (category.total || 0) / totalRevenue * 100
+                percentage: ((category.total || 0) / totalRevenue) * 100 || 0
             }));
 
             const dashboardOverview = {
@@ -163,7 +210,7 @@ class SaleController extends Controller {
                 totalSales: totalSales,
                 averageDailySales: averageDailySales,
                 averageDailyRevenue: averageDailyRevenue,
-                categories: categoriesWithPercentage,
+                categories: categoriesWithPercentage
             };
 
             res.json(dashboardOverview);
@@ -174,37 +221,37 @@ class SaleController extends Controller {
 
     routes = [
         {
-            path: '/',
+            path: "/",
             method: Method.GET,
             handler: this.getAll
         },
         {
-            path: '/month',
+            path: "/month",
             method: Method.GET,
             handler: this.getByMonth
         },
         {
-            path: '/statistics',
+            path: "/statistics",
             method: Method.GET,
-            handler: this.getStatistics,
+            handler: this.getStatistics
         },
         {
-            path: '/:id',
+            path: "/:id",
             method: Method.GET,
             handler: this.getById
         },
         {
-            path: '/',
+            path: "/",
             method: Method.POST,
             handler: this.create
         },
         {
-            path: '/:id',
+            path: "/:id",
             method: Method.DELETE,
             handler: this.delete
         },
         {
-            path: '/bulk',
+            path: "/bulk",
             method: Method.POST,
             handler: this.createMany
         }
--- src/validation/schemas.ts
+++ src/validation/schemas.ts
@@ -1,34 +1,39 @@
 import { z } from "zod";
 
-const NewProductSchema = z.object({
-    id: z.string().optional(),
-    name: z
-        .string()
-        .min(1, { message: "For kort produktnavn" })
-        .max(191, { message: "For langt produktnavn" }),
-    price: z.number().positive({ message: "Pris må ikke være negativ" }),
-    stock: z
-        .number()
-        .min(0, { message: "Lagerbeholdning må ikke være negativ" }),
-    min_stock: z
-        .number()
-        .min(0, { message: "Minimum lagerbeholdning må ikke være negativ" }),
-    max_stock: z.number().min(1, {
-        message: "Maksimum lagerbeholdning må ikke være 0 eller derunder"
-    }),
-    channel: z.enum(["HAIR_CARE", "COSMETIC"], {
-        invalid_type_error: "Channel skal være enten HAIR_CARE eller COSMETIC"
-    }),
-    categories: z
-        .array(
-            z
-                .string()
-                .min(1, { message: "For langt kategorinavn" })
-                .max(191, { message: "For langt kategorinavn" })
-        )
-        .min(1, { message: "For få kategorier (min 1)" })
-        .max(5, { message: "For mange kategorier (max 5)" })
-});
+const NewProductSchema = z
+    .object({
+        id: z.string().optional(),
+        name: z
+            .string()
+            .min(1, { message: "For kort produktnavn" })
+            .max(191, { message: "For langt produktnavn" }),
+        price: z.number().positive({ message: "Pris må ikke være negativ" }),
+        stock: z
+            .number()
+            .min(0, { message: "Lagerbeholdning må ikke være negativ" }),
+        min_stock: z.number().min(0, {
+            message: "Minimum lagerbeholdning må ikke være negativ"
+        }),
+        max_stock: z.number().min(1, {
+            message: "Maksimum lagerbeholdning må ikke være 0 eller derunder"
+        }),
+        channel: z.enum(["HAIR_CARE", "COSMETIC"], {
+            invalid_type_error:
+                "Channel skal være enten HAIR_CARE eller COSMETIC"
+        }),
+        categories: z
+            .array(
+                z
+                    .string()
+                    .min(1, { message: "For langt kategorinavn" })
+                    .max(191, { message: "For langt kategorinavn" })
+            )
+            .min(1, { message: "For få kategorier (min 1)" })
+            .max(5, { message: "For mange kategorier (max 5)" })
+    })
+    .refine((data) => data.min_stock <= data.max_stock, {
+        message: "Minimum lagerbeholdning må ikke være større end maksimum"
+    });
 
 const SaleProductSchema = z.object({
     id: z.string(),
